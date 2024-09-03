8504e1512a98c086e51342ec10f435219999796a
Taero-Kim <taero30@gmail.com>
2024-09-03 20:28:16 +0900

    open graph add type

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,7 @@ export const metadata = {
     description: '2024.10.12(토) 17:00 고려대학교 교우회관 1층 고려스퀘어홀',
     images: '/opengraph-image.jpg',
     url: 'https://taehyun-kihyun.love/',
+    type: 'website',
   },
 };
 
@@ -71,7 +72,7 @@ export default function RootLayout({
         />
         <meta property="og:title" content="김태현 & 김기현 결혼합니다" />
         <meta property="og:site_name" content="김태현 & 김기현 모바일 청첩장" />
-        <meta property="og:url" content="https://taehyun-kihyun.love" />
+        <meta property="og:url" content="https://taehyun-kihyun.love/" />
         <meta
           property="og:description"
           content="2024.10.12(토) 17:00 고려대학교 교우회관 1층 고려스퀘어홀"
