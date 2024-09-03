693b9527b40f77726f23de48f3d65ad84de44d28
Taero-Kim <taero30@gmail.com>
2024-09-03 23:16:47 +0900

    500 error fix

--- component/guestBookWriteDrawer/SubmitButton.jsx
+++ component/guestBookWriteDrawer/SubmitButton.jsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useFormState } from 'react-dom';
+// import { useFormState } from 'react-dom';
 
 import classes from './guestBookWriteDrawer.module.css';
 
-const SubmitButton = () => {
-  const { pending } = useFormState();
+const SubmitButton = ({ loading }) => {
+  // const { pending } = useFormState();
 
   return (
-    <button disabled={pending} type="submit" className={classes.SubmitButton}>
-      {pending ? '저장중...' : '작성하기'}
+    <button disabled={loading} type="submit" className={classes.SubmitButton}>
+      {loading ? '저장중...' : '작성하기'}
     </button>
-  )
-}
+  );
+};
 
 export default SubmitButton;
--- component/guestBookWriteDrawer/index.jsx
+++ component/guestBookWriteDrawer/index.jsx
@@ -10,8 +10,10 @@ import classes from './guestBookWriteDrawer.module.css';
 
 const GuestBookWriteDrawer = ({ isOpen, onClose }) => {
   const [erros, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
 
-  const onSubmit = (formData) => {
+  const onSubmit = async (formData) => {
+    setLoading(true);
     const hasGuestName = !!formData.get('guestName');
     const hasPassword = !!formData.get('password');
     const hasContents = !!formData.get('contents');
@@ -25,7 +27,9 @@ const GuestBookWriteDrawer = ({ isOpen, onClose }) => {
     }
 
     setErrors({});
-    writeGuestBook(formData);
+    await writeGuestBook(formData);
+
+    setLoading(false);
     onClose();
   };
 
@@ -34,7 +38,9 @@ const GuestBookWriteDrawer = ({ isOpen, onClose }) => {
       <form className={classes.form} action={onSubmit}>
         <div className={classes.formItmes}>
           <p className={classes.formItem}>
-            <label className={classes.label} htmlFor="guestName">이름</label>
+            <label className={classes.label} htmlFor="guestName">
+              이름
+            </label>
             <input
               className={classes.textInput}
               type="text"
@@ -42,11 +48,15 @@ const GuestBookWriteDrawer = ({ isOpen, onClose }) => {
               name="guestName"
               placeholder="이름을 입력해주세요"
             />
-            {erros.guestName && <span className={classes.error}>{erros.guestName}</span>}
+            {erros.guestName && (
+              <span className={classes.error}>{erros.guestName}</span>
+            )}
           </p>
 
           <p className={classes.formItem}>
-            <label className={classes.label} htmlFor="password">비밀번호</label>
+            <label className={classes.label} htmlFor="password">
+              비밀번호
+            </label>
             <input
               className={classes.textInput}
               type="password"
@@ -54,27 +64,33 @@ const GuestBookWriteDrawer = ({ isOpen, onClose }) => {
               name="password"
               placeholder="비밀번호를 입력해주세요"
             />
-            {erros.password && <span className={classes.error}>{erros.password}</span>}
+            {erros.password && (
+              <span className={classes.error}>{erros.password}</span>
+            )}
           </p>
 
           <p className={classes.formItem}>
-            <label className={classes.label} htmlFor="contents">방명록</label>
+            <label className={classes.label} htmlFor="contents">
+              방명록
+            </label>
             <textarea
               className={classes.textareaInput}
               id="contents"
               name="contents"
               rows="10"
               maxLength={100}
-              placeholder='100자 이내로 작성해주세요'
+              placeholder="100자 이내로 작성해주세요"
             />
-            {erros.contents && <span className={classes.error}>{erros.contents}</span>}
+            {erros.contents && (
+              <span className={classes.error}>{erros.contents}</span>
+            )}
           </p>
         </div>
 
-        <SubmitButton />
+        <SubmitButton loading={loading} />
       </form>
     </Drawer>
-  )
+  );
 };
 
 export default GuestBookWriteDrawer;
