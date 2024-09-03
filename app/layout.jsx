b6c04f5759dd6f8e9dcf3ab86ce90eafac62b8c1
Taero-Kim <taero30@gmail.com>
2024-09-03 22:48:13 +0900

    og-fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,8 +18,11 @@ export const metadata = {
     siteName: '김태현 & 김기현 모바일청첩장',
     title: '김태현 & 김기현 결혼합니다.',
     description: '2024.10.12(토) 17:00 고려대학교 교우회관 1층 고려스퀘어홀',
-    images: '/opengraph-image.jpg',
-    url: 'https://taehyun-kihyun.love/',
+    images: {
+      url: '/opengraph-image.jpg',
+    },
+    url: 'https://taehyun-kihyun.love',
+    locale: 'ko_KR',
     type: 'website',
   },
 };
@@ -65,21 +68,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, user-scalable=no"
-        />
-        <meta property="og:title" content="김태현 & 김기현 결혼합니다" />
-        <meta property="og:site_name" content="김태현 & 김기현 모바일 청첩장" />
-        <meta property="og:url" content="https://taehyun-kihyun.love/" />
-        <meta
-          property="og:description"
-          content="2024.10.12(토) 17:00 고려대학교 교우회관 1층 고려스퀘어홀"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/opengraph-image.jpg" />
-      </Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, user-scalable=no"
+      />
 
       <body className={gowunBatang.className}>
         <Toaster position="top-center" />
