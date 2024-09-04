14e376fc935e8a316c5744990c7bea72b2145d75
Taero-Kim <taero30@gmail.com>
2024-09-04 18:02:24 +0900

    main-image-replace

--- app/@mainImage/page.jsx
+++ app/@mainImage/page.jsx
@@ -7,13 +7,13 @@ const MainImage = () => {
   return (
     <div className={classes.mainImageContaier}>
       <Image
-        src="/images/0_main_R1.jpg"
+        src="/images/0_main_R1_placeholder.jpg"
         alt="김태현, 김기현의 웨딩 사진 중 메인이미지"
         fill
         priority
         quality={100}
-        blurDataURL="/images/0_main_R1_placeholder.jpg"
-        placeholder="blur"
+        // blurDataURL="/images/0_main_R1_placeholder.jpg"
+        // placeholder="blur"
       />
     </div>
   );
