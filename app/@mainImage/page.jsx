340cf30941e1cf8243ae0d773606cb6e6db8bb0b
Taero-Kim <taero30@gmail.com>
2024-09-04 17:48:28 +0900

    main-img-blur

--- app/@mainImage/page.jsx
+++ app/@mainImage/page.jsx
@@ -4,17 +4,19 @@ import classes from './page.module.css';
 
 // TODO: 이미지 비율에 맞게 설정
 const MainImage = () => {
-    return (
-        <div className={classes.mainImageContaier}>
-            <Image
-                src="/images/0_main_R1.jpg"
-                alt="김태현, 김기현의 웨딩 사진 중 메인이미지"
-                fill
-                priority
-                quality={100}
-            />
-        </div>
-    )
+  return (
+    <div className={classes.mainImageContaier}>
+      <Image
+        src="/images/0_main_R1.jpg"
+        alt="김태현, 김기현의 웨딩 사진 중 메인이미지"
+        fill
+        priority
+        quality={100}
+        blurDataURL="/images/0_main_R1.jpg"
+        placeholder="blur"
+      />
+    </div>
+  );
 };
 
 export default MainImage;
